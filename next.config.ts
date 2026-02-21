054d36257a3f4d6b6a51a9c25a9dafc2d5b899cf
Dsyoll <madalin647t@gmail.com>
2026-02-21 15:09:23 +0200

    fix: remove basePath for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  basePath: "/AcessControl",
-  assetPrefix: "/AcessControl/",
   images: {
     unoptimized: true,
   },
 };
-
 export default nextConfig;
